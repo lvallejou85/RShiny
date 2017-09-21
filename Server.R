c0ea55ddfbc53fbc837940b30a5ef67363025098
lvallejou85 <30954037+lvallejou85@users.noreply.github.com>
2017-09-20 23:17:02 -0500

    Avance Sept 20
    Adición de Menú y botón de  carga de archivo.

--- Server.R
+++ Server.R
@@ -1,34 +1,37 @@
-# Define server logic required to generate and plot a random distribution
 shinyServer(function(input, output) {
   
-  # Expression that generates a plot of the distribution. The expression
-  # is wrapped in a call to renderPlot to indicate that:
-  #
-  #  1) It is "reactive" and therefore should be automatically 
-  #     re-executed when inputs change
-  #  2) Its output type is a plot 
-  #
   output$distPlot <- renderPlot({
     
-    # generate an rnorm distribution and plot it
-    dist <- cumsum(rnorm(input$obs))
-    x <- ts(dist, freq = input$freq, start = c(2010,01))
-    #x <- ts(data = cumsum(rnorm(20)),start = c(1959, 2),frequency = 4) 
-    plot(x,t='l')
+    #Espera la entrada del archivo
+    req(input$archivo)    
+    df <- read.csv(input$archivo$datapath,
+                   header = input$header,
+                   sep = ',',
+                   quote = '"')
     
-#     req(input$file1)
-#     
-#     df <- read.csv(input$file1$datapath,
-#                    header = input$header,
-#                    sep = input$sep,
-#                    quote = input$quote)
-#     
-#     if(input$disp == "head") {
-#       return(head(df))
-#     }
-#     else {
-#       return(df)
-#     }
+    #generate an rnorm distribution and plot it
+    #dist <- cumsum(rnorm(input$obs))
+    #x <- ts(dist, freq = input$freq, start = c(2010,01))
+    #x <- ts(data = cumsum(rnorm(20)),start = c(1959, 2),frequency = 4)
+    
+    columna <- df[2]
+    #SerieTiempo <- ts(columna, freq = input$freq, start = c(2010,01))
+    plot(df,t='l')
+    return(head(df))
 
+  })
+  
+  output$distPlot2 <- renderPlot({
+    
+    req(input$archivo)    
+    df <- read.csv(input$archivo$datapath,
+                   header = input$header,
+                   sep = ',',
+                   quote = '"')
+    
+    columna <- df[2]
+    plot(df,t='l')
+    return(head(df))
+    
   })
 })
--- UI.R
+++ UI.R
@@ -1,45 +1,78 @@
-#shinyServer(
-  
-#  pageWithSidebar(
-    
-#    headerPanel("RShiny App"),sidebarPanel("SideBar"),mainPanel("Main")
-    
-#  )
-  
-#)
 shinyUI(
   
-  pageWithSidebar(
-    
-    #Titulo de la pagina
-    headerPanel("Series de Tiempo - Madagascar"),
-    #Menu del lado izquierdo de la pagina
-    sidebarPanel(
-    
-      #Entrada para la frecuencia de la serie de tiempo
-      sliderInput("freq", 
-                  "Frecuencia de la Serie de Tiempo:", 
-                  min = 1,
-                  max = 12, 
-                  value = 4),
-      #Entrada para el numero de datos de la series aleratoria
-      sliderInput("obs", 
-                  "Observaciones de la serie", 
-                  min = 1,
-                  max = 1000, 
-                  value = 500),
-      tags$hr(),
-      fileInput("archivo", "Choose CSV File",
-                multiple = TRUE,
-                accept = c("text/csv",
-                           "text/comma-separated-values,text/plain",
-                           ".csv")),
-      checkboxInput("header", "¿ Archivo con Cabecera ?", FALSE),
-      tags$hr()    
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotOutput("distPlot")
-  )
+  navbarPage("Series de Tiempo",
+             navbarMenu("Datos",
+                        tabPanel("Cargar",
+                                 fileInput("archivo", "Choose CSV File",
+                                           multiple = TRUE,
+                                           accept = c("text/csv",
+                                                      "text/comma-separated-values,text/plain",
+                                                      ".csv")),
+                                 checkboxInput("header", "¿ Archivo con Cabecera ?", FALSE)
+                                 #Descomentar para que mustre en ambos paneles la gráfica
+                                 # ,
+                                 # mainPanel(
+                                 #   plotOutput("distPlot")
+                                 # )
+                        )
+
+             ),
+             tabPanel("Gráficos",
+                      mainPanel(
+                        plotOutput("distPlot2")
+                      )
+             ),
+             navbarMenu("Modelos"
+
+                        )
+             ),
+             navbarMenu("More",
+                        tabPanel("Table",
+                                 dataTableOutput("table")
+                        ),
+                        tabPanel("About",
+                                 fluidRow(
+                                   column(3,
+                                          img(class="img-polaroid",
+                                              src=paste0("http://upload.wikimedia.org/",
+                                                         "wikipedia/commons/9/92/",
+                                                         "1919_Ford_Model_T_Highboy_Coupe.jpg"))
+                                   )
+                                 )
+                        )
+             )
+  #)
+  # pageWithSidebar(
+  #   
+  #   #Titulo de la pagina
+  #   headerPanel("Series de Tiempo - Madagascar"),
+  #   #Menu del lado izquierdo de la pagina
+  #   sidebarPanel(
+  #   
+  #     #Entrada para la frecuencia de la serie de tiempo
+  #     sliderInput("freq", 
+  #                 "Frecuencia de la Serie de Tiempo:", 
+  #                 min = 1,
+  #                 max = 12, 
+  #                 value = 4),
+  #     #Entrada para el numero de datos de la series aleratoria
+  #     sliderInput("obs", 
+  #                 "Observaciones de la serie", 
+  #                 min = 1,
+  #                 max = 1000, 
+  #                 value = 500),
+  #     tags$hr(),
+  #     fileInput("archivo", "Choose CSV File",
+  #               multiple = TRUE,
+  #               accept = c("text/csv",
+  #                          "text/comma-separated-values,text/plain",
+  #                          ".csv")),
+  #     checkboxInput("header", "¿ Archivo con Cabecera ?", FALSE),
+  #     tags$hr()    
+  # ),
+  # 
+  # # Show a plot of the generated distribution
+  # mainPanel(
+  #   plotOutput("distPlot")
+  # )
 ))
